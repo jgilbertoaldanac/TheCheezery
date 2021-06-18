2be104dfea9a49f43bd0aa23ebe4305ebf46f257
Gilberto Aldana <78342937+jgilbertoaldanac@users.noreply.github.com>
2021-06-18 10:23:44 -0700

    1

---
+++ app/src/main/java/aldana/jesus/thecheezery/Product.kt
@@ -0,0 +1,7 @@
+package aldana.jesus.thecheezery
+
+data class Product(var name: String,
+                   var image: Int,
+                   var description: String,
+                   var price: Double){
+}
